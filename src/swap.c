0214493b4f71b2c2f1b47b3c09a1cb0ffa6ce1d5
Thanh-cong VO <tvo@e3r1p5.clusters.42paris.fr>
2023-03-28 22:29:08 +0200

    swap ok

--- src/swap.c
+++ src/swap.c
@@ -6,19 +6,19 @@
 /*   By: tvo <tvo@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/20 18:10:06 by tvo               #+#    #+#             */
-/*   Updated: 2023/03/28 22:21:46 by tvo              ###   ########.fr       */
+/*   Updated: 2023/03/28 22:26:39 by tvo              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../push_swap.h"
 
-t_head	*swap_a(t_head *head)
+t_pile	*swap_a(t_head *head)
 {
 	t_pile	*pile_a = head->pile_a;
 	int	tmp;
 
 	if (pile_a == NULL || pile_a->next == NULL)
-		return pile_a;
+		return (pile_a);
 	tmp = pile_a->data;
 	pile_a->data = pile_a->next->data;
 	pile_a->next->data = tmp;
@@ -32,7 +32,7 @@ t_pile	*swap_b(t_head *head)
 	int	tmp;
 
 	if (pile_b == NULL || pile_b->next == NULL)
-		return pile_b;
+		return (pile_b);
 	tmp = pile_b->data;
 	pile_b->data = pile_b->next->data;
 	pile_b->next->data = tmp;
@@ -40,9 +40,9 @@ t_pile	*swap_b(t_head *head)
 	return (pile_b);
 }
 
-void	swap_ss(t_head *head)
-{
-	swap_a(head);
-	swap_b(head);
-	printf("ss");
-}
+// void	swap_ss(t_head *head)
+// {
+// 	swap_a(head);
+// 	swap_b(head);
+// 	printf("ss");
+// }
