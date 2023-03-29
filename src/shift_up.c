f1409d69cef79e2d1f599d15396fae33b968e35d
Thanh-cong VO <tvo@e3r1p5.clusters.42paris.fr>
2023-03-29 17:16:19 +0200

    shift_up functions-v2 is ok

--- src/shift_up.c
+++ src/shift_up.c
@@ -1,54 +1,35 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   rotate.c                                           :+:      :+:    :+:   */
+/*   shift_up.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tvo <tvo@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/29 02:26:45 by tvo               #+#    #+#             */
-/*   Updated: 2023/03/29 03:07:15 by tvo              ###   ########.fr       */
+/*   Updated: 2023/03/29 17:15:46 by tvo              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../push_swap.h"
 
-// void	rotate_a(t_head *head)
-// {
-// 	if (head->pile_a != NULL)
-// 	{
-// 		int tmp;
-
-// 		tmp = head->pile_a->data;
-// 		head->pile_a = add_last_pile(head, tmp);
-// 	}
-// }
-
 void shift_up(t_head *head)
 {
-    // Vérifier si la pile a est vide
-    if (head->pile_a == NULL)
-    {
-        printf("La pile a est vide\n");
-        return;
-    }
-
-    // Déplacer le premier élément de la pile a vers la fin
-    t_pile *current = head->pile_a;
-    head->pile_a = head->pile_a->next;
-
-    // Ajouter l'élément au bas de la pile a
-    current->next = NULL;
-    t_pile *last = head->pile_a;
-    if (last == NULL)
-    {
-        head->pile_a = current;
-    }
-    else
-    {
-        while (last->next != NULL)
-        {
-            last = last->next;
-        }
-        last->next = current;
-    }
+	if (head->pile_a == NULL)
+	{
+		printf("Pile a est vide!");
+		return ;
+	}
+	t_pile	*current = head->pile_a;
+	if (current->next == NULL)
+	{
+		return;
+	}
+	while (current->next != NULL)
+	{
+		current = current->next;
+	}
+	current->next = head->pile_a;
+	head->pile_a = head->pile_a->next;
+	current->next->next = NULL;
 }
+
